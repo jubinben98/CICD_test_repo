e125a55d55509d5fbb1ddca9618f272183794509
jubinben98 <jubinben.666@gmail.com>
2021-05-18 21:56:10 +0530

    Update Random.py

--- test_folder/Random.py
+++ test_folder/Random.py
@@ -1,5 +1,6 @@
 def calculate(a, b):
   print("Got the first argument: ", a)
   print("Got the second argument: ", b)
+  print("Result for addition is: ", a+b)
   print("Result is: ", a*b)
   return a*b
